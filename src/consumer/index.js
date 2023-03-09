51fb193df49ee28750c6aa7ff830ee5653ea936c
davidpestana <david.pestana.perdomo@gmail.com>
2023-03-09 10:47:34 +0100

    improve operations

--- src/consumer/index.js
+++ src/consumer/index.js
@@ -5,4 +5,23 @@ const kafka = new Kafka({
   brokers: ['broker1:9092', 'broker2:9092', 'broker3:9092'],
 })
 
-console.log('todo implement consumer');
+const consumer = kafka.consumer({ groupId: 'test-group' })
+
+
+
+
+const listen = async () => { 
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        value: message.value.toString(),
+      })
+    },
+  })
+}
+
+
+listen().then();
